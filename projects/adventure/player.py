baa5b01ae432da275570c3a4e5548f6f2bdd5054
Brandon Porter <brendlorend@gmail.com>
2019-11-07 23:43:25 -0800

    cleanup

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -2,18 +2,12 @@ class Player:
     def __init__(self, name, startingRoom):
         self.name = name
         self.currentRoom = startingRoom
-        self.done=[]
-    def travel(self, direction, path, showRooms = False):
+    def travel(self, direction, showRooms = False):
         nextRoom = self.currentRoom.getRoomInDirection(direction)
-        print(f"from {self.currentRoom.id} to {nextRoom.id if nextRoom else 'None'}")
         if nextRoom is not None:
-            self.done.append(direction)
             self.currentRoom = nextRoom
             if (showRooms):
                 nextRoom.printRoomDescription(self)
         else:
             print("You cannot move in that direction.")
-            print(f"data {self.currentRoom.id} heading {direction}")
-            print(self.done)
-            print(path)
             
