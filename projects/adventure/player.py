fbd761216d44a2611292240972ca90530da028fa
Brandon Porter <brendlorend@gmail.com>
2019-11-07 23:41:11 -0800

    done with stretch goal reached 987

--- projects/adventure/player.py
+++ projects/adventure/player.py
@@ -2,11 +2,18 @@ class Player:
     def __init__(self, name, startingRoom):
         self.name = name
         self.currentRoom = startingRoom
-    def travel(self, direction, showRooms = False):
+        self.done=[]
+    def travel(self, direction, path, showRooms = False):
         nextRoom = self.currentRoom.getRoomInDirection(direction)
+        print(f"from {self.currentRoom.id} to {nextRoom.id if nextRoom else 'None'}")
         if nextRoom is not None:
+            self.done.append(direction)
             self.currentRoom = nextRoom
             if (showRooms):
                 nextRoom.printRoomDescription(self)
         else:
             print("You cannot move in that direction.")
+            print(f"data {self.currentRoom.id} heading {direction}")
+            print(self.done)
+            print(path)
+            
